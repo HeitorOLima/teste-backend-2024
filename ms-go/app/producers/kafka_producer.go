17379715a58b2a4ae8b3ed8ff42a6c585dcea9e5
Heitor Lima <ho_lima@outlook.com>
2024-07-02 16:45:01 -0300

    [MS-GO] Implementado o kafka producer

---
+++ ms-go/app/producers/kafka_producer.go
@@ -0,0 +1,56 @@
+package KafkaProducer
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var (
+	producerInstance *KafkaProducer
+	once             sync.Once
+)
+
+type KafkaProducer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer(brokers []string, topic string) *KafkaProducer {
+	writer := &kafka.Writer{
+		Addr:                   kafka.TCP(brokers...),
+		Topic:                  topic,
+		Balancer:               &kafka.LeastBytes{},
+		AllowAutoTopicCreation: true,
+		MaxAttempts:            5,
+	}
+	return &KafkaProducer{writer: writer}
+}
+
+func GetProducer() *KafkaProducer {
+	once.Do(func() {
+		producerInstance = NewProducer([]string{"localhost:9092"}, "go-to-rails")
+	})
+	return producerInstance
+}
+
+func (producer *KafkaProducer) Close() {
+	producer.writer.Close()
+}
+
+func (producer *KafkaProducer) ProduceMessage(key, value []byte) error {
+	msg := kafka.Message{
+		Key:   key,
+		Value: value,
+	}
+
+	err := producer.writer.WriteMessages(context.Background(), msg)
+	if err != nil {
+		log.Printf("Erro ao produzir mensagem: %v", err)
+	} else {
+		log.Printf("Mensagem produzida: %s", value)
+	}
+
+	return err
+}
