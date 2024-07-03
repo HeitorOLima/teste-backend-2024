059c1edd34461f19dee530ec70321cf232edb67b
HeitorOLima <ho_lima@outlook.com>
2024-07-03 00:31:36 -0300

    [MS-RAILS] Ajuste na classe ApplicationConsumer

--- ms-rails/app/consumers/application_consumer.rb
+++ ms-rails/app/consumers/application_consumer.rb
@@ -4,6 +4,4 @@
 # You can rename it if it would conflict with your current code base (in case you're integrating
 # Karafka with other frameworks)
 class ApplicationConsumer < Karafka::BaseConsumer
-  include Karafka::Backends::Retry
-  self.retry_policy = { interval: 5, max_retries: 10 }
 end
